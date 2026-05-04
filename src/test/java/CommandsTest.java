bbccb1a0fb0565ff62c645170a4ee0f0ca05288c
Daniel <daniel.stampfli@unifr.ch>
2026-05-05 00:30:01 +0200

    Add build and test files

---
+++ src/test/java/CommandsTest.java
@@ -0,0 +1,29 @@
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+import picocli.CommandLine;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CommandsTest {
+    @Test
+    public void testURLOptionIsParsed() {
+        String[] args = {"--url", "http://localhost:3000/shop-api", "list"};
+        ShopCli app = new ShopCli();
+
+        app.parse(args);
+
+        assertEquals("http://localhost:3000/shop-api", app.getUrl());
+    }
+
+    @Test
+    public void testListCommandWithJsonFormat() {
+        ShopCli app = new ShopCli();
+        CommandLine cmd = new CommandLine(app);
+
+        cmd.parseArgs("list", "--format", "json");
+
+        ListCommand listCmd = cmd.getSubcommands().get("list").getCommand();
+        assertEquals("json", listCmd.getFormat());
+    }
+}
